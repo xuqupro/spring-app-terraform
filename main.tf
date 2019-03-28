1fdc79df4d4b2fb4def3bc33fd74aac1f7a85933
xuqupro <thegioiitjob@gmail.com>
2019-03-28 23:52:37 +0700

    message

--- main.tf
+++ main.tf
@@ -63,13 +63,13 @@ resource "aws_security_group" "elbsg" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-egress {
+  egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-lifecycle {
+  lifecycle {
     create_before_destroy = true
   }
 }
